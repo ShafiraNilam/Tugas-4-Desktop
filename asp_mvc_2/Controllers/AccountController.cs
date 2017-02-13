2c6ddaa9bb87427a37e977fc259f5478dcd9bcfd
Shafira Nilamsari M <ns369shafira@gmail.com>
2017-02-13 09:24:11 +0700

    commit finish

--- asp_mvc_2/Controllers/AccountController.cs
+++ asp_mvc_2/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using System.Web.Security;
 using Asp_mvc_2.Models.ViewModel;
 using Asp_mvc_2.Models.EntityManager;
+using asp_mvc_2.Models.ViewModel;
 namespace Asp_mvc_2.Controllers
 {
     public class AccountController : Controller
@@ -18,6 +19,25 @@ namespace Asp_mvc_2.Controllers
                     return RedirectToAction("Welcome", "Home"); 
                 } else 
                     ModelState.AddModelError("", "Login Name already taken."); } 
-            return View(); }
+            return View(); 
+        }
+        public ActionResult LogIn() { 
+            return View(); 
+        } 
+        [HttpPost] 
+        public ActionResult LogIn(UserLoginView ULV, string returnUrl) {
+            if (ModelState.IsValid) {
+                UserManager UM = new UserManager();
+                string password = UM.GetUserPassword(ULV.LoginName); 
+                if (string.IsNullOrEmpty(password)) ModelState.AddModelError("", "The user login or password provided is incorrect."); 
+                else { 
+                    if (ULV.Password.Equals(password)) {
+                        FormsAuthentication.SetAuthCookie(ULV.LoginName, false); 
+                        return RedirectToAction("Welcome", "Home"); } 
+                    else { 
+                        ModelState.AddModelError("", "The password provided is incorrect."); } } } // If we got this far, something failed, redisplay form 
+            return View(ULV); }
+        [Authorize]
+        public ActionResult SignOut() { FormsAuthentication.SignOut(); return RedirectToAction("Index", "Home"); }
     }
 }
